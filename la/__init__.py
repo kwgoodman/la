5ffc5add33a5052ee4a36f3fc6837fdf207b400e
kg <>
2010-01-02 16:01:28 +0000

    * io/hdf5.py, __init__.py: New module for saving and loading larrys in       HDF5 format.

--- la/__init__.py
+++ la/__init__.py
@@ -5,6 +5,11 @@ from la.deflarry import larry
 del deflarry  # Remove deflarry from namespace
 
 from la.io.npz import save_npz, load_npz
+try:
+    from la.io.hdf5 import save_hdf5, load_hdf5
+except:
+    # I assume you haven't installed h5py and hdf5
+    pass    
 
 try:
     from numpy.testing import Tester
