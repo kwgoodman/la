1b934eb465d54f9a85c2f95cdbdfdd3e957b46bc
kg <>
2010-01-04 22:37:31 +0000

    * io/tests, io/tests/io_test.py: Made a unit test module for io with a few       unit tests in it.

---
+++ la/io/tests/io_test.py
@@ -0,0 +1,65 @@
+"io unit tests."
+
+import unittest
+import tempfile
+import os
+
+import numpy as np
+nan = np.nan
+
+from la import larry
+from la import IO
+
+
+class Test_io(unittest.TestCase):
+    "Test io."
+    
+    def setUp(self):
+        suffix = '.hdf5'
+        prefix = 'la_io_unittest'
+        self.filename = tempfile.mktemp(suffix=suffix, prefix=prefix)
+        
+    def tearDown(self):
+        os.unlink(self.filename)
+        
+    def test_io_1(self):
+        "io_general"
+        io = IO(self.filename)
+        x = larry([1,2,3]) 
+        io['x'] = x
+        self.assert_('x' in io, 'key missing')
+        self.assert_((x == io['x']).all(), 'save and load difference')
+        self.assert_(['x'] == io.keys(), 'keys are different')
+        self.assert_(x.dtype == io['x'].dtype, 'dtype changed')
+        del io['x']
+        self.assert_(io.keys() == [], 'key still present')
+        
+    def test_io_2(self):
+        "io_repack"
+        io = IO(self.filename)                
+        io['larry'] = larry(np.random.rand(100,100))
+        fs1 = io.freespace()
+        sp1 = io.space()
+        del io['larry']
+        io.repack()
+        fs2 = io.freespace()
+        sp2 = io.space()
+        self.assert_(fs2 < fs1, 'repack did not reduce freespace')
+        self.assert_(sp2 < sp1, 'repack did not reduce space')
+     
+        
+def testsuite():
+    s = []
+    u = unittest.TestLoader().loadTestsFromTestCase
+    s.append(u(Test_io))
+    return unittest.TestSuite(s)
+
+def run():   
+    suite = testsuite()
+    unittest.TextTestRunner(verbosity=2).run(suite)
+    
+if __name__ == '__main__':
+    run()           
+        
+    
+          
