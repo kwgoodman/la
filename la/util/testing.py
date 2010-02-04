e61182fca75a58b4d201dd6d33af9e0e06052aa7
Keith Goodman <kwgoodman@gmail.com>
2010-02-04 13:15:37 -0800

    * util.testing.py(assert_larry_equal), util.testing.py(heading):       New function to assert equality of two larrys.
        * RELEASE: Added assert_larry_equal.

--- la/util/testing.py
+++ la/util/testing.py
@@ -1,6 +1,61 @@
 "LA unit test utility functions."
 
+from numpy.testing import assert_, assert_equal, assert_almost_equal
+
 from la import larry
+
+def assert_larry_equal(actual, desired, msg='', dtype=True, original=None,
+                       noreference=True, nocopy=False, verbose=True):
+    """Assert equality of attributes of two larries."""
+    
+    fail = []
+    
+    # label
+    try:         
+        assert_equal(actual.label, desired.label)
+    except AssertionError, err:
+        fail.append(heading('LABEL') + str(err))      
+
+    # Data array
+    try:  
+        assert_equal(actual.x, desired.x)
+    except AssertionError, err:
+        fail.append(heading('X DATA ARRAY') + str(err))
+     
+    # dtype
+    if dtype: 
+        try: 
+            assert_equal(actual.x.dtype, desired.x.dtype)
+        except AssertionError, err:
+            fail.append(heading('DTYPE') + str(err))            
+    
+    # Check for references or copies
+    if noreference:
+        if original is None:
+            raise ValueError, 'original must be a larry to run noreference check.'
+        try:
+            assert_(assert_noreference(actual, original))
+        except AssertionError, err:
+            fail.append(heading('REFERENCE FOUND') + str(err))               
+    elif nocopy:
+        if original is None:
+            raise ValueError, 'original must be a larry to run nocopy check.' 
+        try:       
+            assert_(assert_nocopy(actual, original))
+        except AssertionError, err:
+            fail.append(heading('COPY INSTEAD OF REFERENCE FOUND') + str(err))              
+    else:   #FIXME check view for different dimensional larries
+        pass
+    
+    # Did the test pass?    
+    if len(fail) > 0:
+        # No
+        msg = ''.join(fail)
+        raise AssertionError, msg
+
+def heading(text):
+    line = '-' * len(text)
+    return '\n\n' + line + '\n' + text + '\n' + line + '\n'
     
 def printfail(theory, practice, header=None):
     x = []
