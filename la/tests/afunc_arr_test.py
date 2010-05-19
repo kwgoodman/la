8d666157ceecb93db3a7ef55166178361ca39479
Keith Goodman <kwgoodman@gmail.com>
2010-05-19 06:45:27 -0700

    * afunc.py(nans), tests/afunc_arr_test.py, RELEASE, doc/source/intro.rst:       Bug #582579: nans now fills with '' for str dtype and None for object       dtype. Doc string and unit tests added.

--- la/tests/afunc_arr_test.py
+++ la/tests/afunc_arr_test.py
@@ -3,13 +3,13 @@
 import unittest
 
 import numpy as np
-from numpy.testing import assert_almost_equal
+from numpy.testing import assert_almost_equal, assert_equal, assert_raises
 nan = np.nan
 
 from la.util.testing import printfail
 from la.afunc import group_ranking, group_mean, group_median
 from la.afunc import (movingsum, movingrank, movingsum_forward, ranking, 
-                      geometric_mean, unique_group)
+                      geometric_mean, unique_group, nans)
 
 # Sector functions ----------------------------------------------------------
 
@@ -637,8 +637,68 @@ class Test_movingrank(unittest.TestCase):
                            [nan,  1.0],
                            [nan, -1.0]])
         actual = movingrank(self.x2, self.window)
-        assert_almost_equal(actual, desired)           
+        assert_almost_equal(actual, desired) 
+        
 
+# NaN functions -------------------------------------------------------------                  
+
+class Test_nans(unittest.TestCase):
+    "Test nans"                 
+
+    def test_nans_1(self):
+        "afunc.nans_1"
+        shape = (2,)
+        actual = nans(shape)    
+        desired = np.array([nan, nan])
+        assert_almost_equal(actual, desired) 
+    
+    def test_nans_2(self):
+        "afunc.nans_2"
+        shape = (2,)
+        dtype = float
+        actual = nans(shape, dtype)    
+        desired = np.array([nan, nan]) 
+        assert_almost_equal(actual, desired)       
+
+    def test_nans_3(self):
+        "afunc.nans_3"
+        shape = (2,)
+        dtype = str
+        actual = nans(shape, dtype)    
+        desired = np.array(['', ''])
+        assert_equal(actual, desired) 
+
+    def test_nans_4(self):
+        "afunc.nans_4"
+        shape = (2,)
+        dtype = object
+        actual = nans(shape, dtype)
+        desired = np.array([None, None])
+        assert_equal(actual, desired)
+
+    def test_nans_5(self):
+        "afunc.nans_5"
+        shape = (2, 4, 3)
+        dtype = object
+        actual = nans(shape, dtype)    
+        desired = np.zeros(shape, dtype=dtype)
+        desired[:] = None
+        assert_equal(actual, desired)
+
+    def test_nans_6(self):
+        "afunc.nans_6"
+        shape = 0
+        dtype = str
+        actual = nans(shape, dtype)    
+        desired = np.zeros(shape, dtype=dtype)
+        assert_equal(actual, desired) 
+
+    def test_nans_7(self):
+        "afunc.nans_7"
+        shape = 0
+        dtype = int
+        assert_raises(TypeError, nans, shape, dtype)
+        
 
 # Unit tests ----------------------------------------------------------------        
     
@@ -657,7 +717,10 @@ def suite():
     s.append(unit(Test_geometric_mean))
     s.append(unit(Test_movingsum))
     s.append(unit(Test_movingsum_forward))
-    s.append(unit(Test_movingrank))      
+    s.append(unit(Test_movingrank)) 
+    
+    # NaN functions
+    s.append(unit(Test_nans))          
          
     return unittest.TestSuite(s)
 
