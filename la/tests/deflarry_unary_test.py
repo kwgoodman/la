b5612dee0b06037714ccee357a870c19f9d81756
Keith Goodman <kwgoodman@gmail.com>
2012-04-28 15:04:08 -0700

    REf Unit test refactor of unary methods completed.

--- la/tests/deflarry_unary_test.py
+++ la/tests/deflarry_unary_test.py
@@ -44,8 +44,6 @@ def arrays(dtypes=DTYPES, nans=True):
                     yield -a
                 """    
 
-# Unary functions -----------------------------------------------------------
-
 def unit_maker(name, npfunc, arg={'la':[], 'np':[]}, imag=True):
     "Test that larry.xxx gives the same output as np.xxx."
     msg = '\nfunc %s | input %s (%s) | shape %s | axis %s\n'
@@ -61,86 +59,98 @@ def unit_maker(name, npfunc, arg={'la':[], 'np':[]}, imag=True):
         ale(actual, desired, msg=name, original=original)
 
 def test_log():
-    "Test log"
     yield unit_maker, "log", np.log
 
 def test_exp():
-    "Test exp"
     yield unit_maker, "exp", np.exp
 
 def test_sqrt():
-    "Test sqrt"
     yield unit_maker, "sqrt", np.sqrt
 
 def test_sign():
-    "Test sign"
     yield unit_maker, "sign", np.sign
 
 def test_power():
-    "Test power"
     yield unit_maker, "power", np.power, {'la':[1.3], 'np':[1.3]}
 
 def test___pow__():
-    "Test __pow__"
     yield unit_maker, "__pow__", np.power, {'la':[1.3], 'np':[1.3]}
 
 def test_cumsum():
-    "Test cumsum"
     yield unit_maker, "cumsum", np.cumsum, {'la':[0], 'np':[0]}
     yield unit_maker, "cumsum", np.cumsum, {'la':[-1], 'np':[-1]}
 
 def test_cumprod():
-    "Test cumprod"
     yield unit_maker, "cumprod", np.cumprod, {'la':[0], 'np':[0]}
     yield unit_maker, "cumprod", np.cumprod, {'la':[-1], 'np':[-1]}
 
 def test_clip():
-    "Test clip"
     yield unit_maker, "clip", np.clip, {'la':[-1,1], 'np':[-1,1]}
 
 def test___neg__():
-    "Test __neg__"
     yield unit_maker, "__neg__", np.negative
 
-def positive(a):
-    return a.copy()
-
 def test___pos__():
-    "Test __pos__"
+    def positive(a):
+        return a.copy()
     yield unit_maker, "__pos__", positive
 
 def test_abs():
-    "Test abs"
     yield unit_maker, "abs", np.absolute, {'la':[], 'np':[]}, False
 
 def test___abs__():
-    "Test __abs__"
     yield unit_maker, "__abs__", np.absolute, {'la':[], 'np':[]}, False
 
 def test_isnan():
-    "Test isnan"
     yield unit_maker, "isnan", np.isnan
 
 def test_isfinite():
-    "Test isfinite"
     yield unit_maker, "isfinite", np.isfinite
 
 def test_isinf():
-    "Test isinf"
     yield unit_maker, "isinf", np.isinf
 
+def test_nan_replace():
+    def nanreplace(a, w=0):
+        a = a.copy()
+        a[np.isnan(a)] = w
+        return a
+    yield unit_maker, "nan_replace", nanreplace
+
 def test_invert():
-    "larry.invert"
     o = la.larry([True, False])
     d = la.larry([False, True])
     ale(o.invert(), d, 'invert', original=o)                  
-    y = la.larry([0, 1])
-    assert_raises(TypeError, y.invert)      
 
 def test___invert__():
-    "larry.__invert__"
     o = la.larry([True, False])
     d = la.larry([False, True])
     ale(~o, d, '__invert__', original=o)
+
+# assert raises -------------------------------------------------------------
+
+def test_cumsum_raises():
+    "cumsum raises"
     y = la.larry([0, 1])
-    assert_raises(TypeError, y.invert)    
+    assert_raises(ValueError, y.cumsum, None)      
+
+def test_cumprod_raises():
+    "cumprod raises"
+    y = la.larry([0, 1])
+    assert_raises(ValueError, y.cumprod, None)      
+
+def test_clip_raises():
+    "clip raises"
+    y = la.larry([0, 1])
+    assert_raises(ValueError, y.clip, 3, 2)      
+
+def test_invert_raises():
+    "invert raises"
+    y = la.larry([0, 1])
+    assert_raises(TypeError, y.invert)
+
+def test___invert__raises():
+    "invert raises"
+    y = la.larry([0, 1])
+    assert_raises(TypeError, y.invert)
+
